697aa42a8960c7732fcfee2727273988f92d8cbe
Angus Thomsen <aksthomsen@gmail.com>
2016-05-11 19:24:00 +1000

    parsing test

--- src/choice_vec.rs
+++ src/choice_vec.rs
@@ -1,14 +1,25 @@
 use util::HASH_SIZE;
+use relation::{Relation};
 
+pub type ChoiceEntry = (u64, u8);
+
+#[derive(Debug)]
 pub struct ChoiceVec {
-    pub data: [(u64, u8); HASH_SIZE]
+    pub data: [ChoiceEntry; HASH_SIZE],
+    pub attr_number: u64
+}
+
+enum ParseError {
+    NumberError,
+    MissingNumError
 }
 
 impl ChoiceVec {
-    pub fn new(given_bits: Vec<(u64, u8)>, num_attrs: u64) -> ChoiceVec {
+    pub fn new(given_bits: Vec<ChoiceEntry>, attrs: u64) -> ChoiceVec {
         assert!(given_bits.len() <= HASH_SIZE);
         let mut cv = ChoiceVec {
-            data: [(0, 0); HASH_SIZE]
+            data: [(0, 0); HASH_SIZE],
+            attr_number: attrs
         };
         // Copy in the provided bits.
         cv.data[0 .. given_bits.len()].clone_from_slice(&given_bits[..]);
@@ -16,4 +27,46 @@ impl ChoiceVec {
         // TODO: Generate the rest.
         cv
     }
+
+    fn parse_choice_vec(input: &str, attrs: u64) -> Result<ChoiceVec, ParseError> {
+        let mut c_vector = ChoiceVec {
+            data: [(0, 0); HASH_SIZE],
+            attr_number: attrs
+        };
+
+        for (i, entry) in input.split(':').enumerate() {
+            let mut split = entry.split(',');
+            let lo = split.nth(0);
+            let ro = split.nth(1);
+            if let (Some(l_str), Some(r_str)) = (lo, ro) {
+                let l = try!(l_str.parse::<u64>().or_else(|_| Err(ParseError::NumberError)));
+                let r = try!(r_str.parse::<u8>().or_else(|_| Err(ParseError::NumberError)));
+                c_vector.data[i] = (l, r);
+            }
+            else {
+                return Err(ParseError::MissingNumError)
+            }
+        }
+
+        Ok(c_vector)
+    }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::{ChoiceVec, ChoiceEntry};
+    use util::HASH_SIZE;
+
+    #[test]
+    fn parse_1_n_2s() {
+       if let Ok(result) = ChoiceVec::parse_choice_vec("1,2:1,2", 2) {
+         let expect     = ChoiceVec::new(vec![(1, 2), (1, 2)], 2);
+         assert_eq!(result.data, expect.data);
+       }
+       else {
+         panic!("failed to parse");
+       }
+    }
+}
+
